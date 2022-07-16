ca1de8cdfb8651d5ed1bac04b45ea61026b72066
rafaelholland <rafael.holland@bandtec.com.br>
2022-07-16 18:58:45 -0300

    date select

--- sds/src/main/java/com/sdsmeta/demo/controllers/SaleController.java
+++ sds/src/main/java/com/sdsmeta/demo/controllers/SaleController.java
@@ -3,9 +3,13 @@ package com.sdsmeta.demo.controllers;
 import com.sdsmeta.demo.entities.Sale;
 import com.sdsmeta.demo.services.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.List;
 
@@ -17,8 +21,11 @@ public class SaleController {
     private SaleService service;
 
     @GetMapping
-    public List<Sale> findSales(){
-        return service.findSales();
+    public Page<Sale> findSales(
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
+            Pageable pageable){
+        return service.findSales(minDate, maxDate, pageable);
     }
 
 }
